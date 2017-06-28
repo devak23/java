cf47a9b75ade093022a0551cd057ae249b186b64
Abhay Kulkarni <developer.ak23@gmail.com>
2017-06-28 08:40:28 +0530

    Added sorting logic to the map

--- nioexamples/src/main/java/com/ak/learning/nio/WordCounter.java
+++ nioexamples/src/main/java/com/ak/learning/nio/WordCounter.java
@@ -3,23 +3,27 @@ package com.ak.learning.nio;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WordCounter {
     private String content = null;
+    private long count = 0;
+
+    public long getCount() {
+        return count;
+    }
 
     public static void main(String[] args) {
         WordCounter counter = new WordCounter();
         System.out.println("================= WORD COUNT ====================");
         Map<String, Integer> countWords = counter.countWords("sherlock.txt");
         System.out.println("Word count = " + countWords);
+        System.out.println("Total word count = " + counter.getCount());
 
         Map<String, Integer> largeWords = counter.countWords("sherlock.txt", 7);
         System.out.println("================= LARGE WORDS ======================");
         System.out.println("Word Count = " + largeWords);
+        System.out.println("Total word count = " + counter.getCount());
     }
 
     private Map<String, Integer> countWords(String file, int largeCount) {
@@ -32,17 +36,18 @@ public class WordCounter {
         }
 
         List<String> words = Arrays.asList(content.split("\\P{L}+"));
+        this.count = words.size();
         words.forEach(w -> {
             if (w.length() > largeCount) {
                 wordCount.merge(w, 1, Integer::sum);
             }
         });
 
-        return wordCount;
+        return sortByValues(wordCount);
     }
 
     private Map<String, Integer> countWords(String file) {
-        Map<String, Integer> wordCount = new HashMap<String, Integer>();
+        Map<String, Integer> wordCount = new HashMap<>();
         if (content == null) {
             readFile(file);
         }
@@ -51,11 +56,12 @@ public class WordCounter {
             return wordCount;
         }
         List<String> words = Arrays.asList(content.split("\\P{L}+"));
+        this.count = words.size();
         words.forEach (word -> {
            wordCount.merge(word, 1, Integer::sum);
         });
 
-        return wordCount;
+        return sortByValues(wordCount);
 
     }
 
@@ -65,5 +71,23 @@ public class WordCounter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**  To sort the TreeMap by values */
+    private static <K,V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
+        Comparator<K> valueComparator = new Comparator<K>() {
+            @Override
+            public int compare(K key1, K key2) {
+                int comparison = map.get(key1).compareTo(map.get(key2));
+                if (comparison == 0) {
+                    return 1;
+                } else {
+                    return comparison;
+                }
+            }
+        };
+        Map<K,V> sortedByValues = new TreeMap<>(valueComparator);
+        sortedByValues.putAll(map);
+        return sortedByValues;
     }
 }
