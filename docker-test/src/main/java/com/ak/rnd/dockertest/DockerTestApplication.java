301b2ffda9c0ecaab3cb5ec585b195551f6f7353
devak23 <developer.ak23@gmail.com>
2020-01-10 05:20:33 +0530

    Fixed the issue with ComponentScan annotation

--- docker-test/src/main/java/com/ak/rnd/dockertest/DockerTestApplication.java
+++ docker-test/src/main/java/com/ak/rnd/dockertest/DockerTestApplication.java
@@ -2,7 +2,9 @@ package com.ak.rnd.dockertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.ak.rnd.resource")
 @SpringBootApplication
 public class DockerTestApplication {
 
--- docker-test/src/main/java/com/ak/rnd/resource/HelloResource.java
+++ docker-test/src/main/java/com/ak/rnd/resource/HelloResource.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/docker/v1")
 public class HelloResource {
 
-    @GetMapping("/")
+    @GetMapping("/greet")
     public String greet() {
         return "Hello From SpringBoot!";
     }
