5d22137f471fb78d2039aa2b0e34de4d66fe6017
Abhay Kulkarni <developer.ak23@gmail.com>
2024-01-26 14:27:16 +0530

    Mono from runnable

---
+++ Reactive/src/main/java/com/ak/reactive/utils/lectures/Lec08MonoFromRunnable.java
@@ -0,0 +1,33 @@
+package com.ak.reactive.utils.lectures;
+
+import com.ak.reactive.utils.Util;
+import reactor.core.publisher.Mono;
+
+/**
+ * A Runnable interface doesn't accept any parameter and doesn't return any parameter. Can that be used in any way
+ * in this publisher business? Yes. There are many situations (long-running tasks) where you might need a notification of
+ * a task completed. A Mono from Runnable becomes useful in those situations
+ */
+public class Lec08MonoFromRunnable {
+
+    // Let's first create a long running task
+    private static Runnable runTimeConsumingOperation() {
+        return () -> {
+            System.out.println("Time consuming operation started...");
+            Util.sleep(5);
+            System.out.println("Time consuming operation ended.");
+        };
+    }
+
+    // Now let's create a Mono that can use this data source and turn into a stream.
+    public static void main(String[] args) {
+        System.out.println("******** Begin *********");
+        Mono.fromRunnable(runTimeConsumingOperation())
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        () -> System.out.println("Time consuming operation has completed. Now sending emails...")
+                );
+        System.out.println("******** End *********");
+    }
+}
