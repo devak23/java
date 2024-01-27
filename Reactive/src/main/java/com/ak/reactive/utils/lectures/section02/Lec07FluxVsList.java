fc566a06a66ea552cdc691b22088db820ea1bba6
Abhay Kulkarni <developer.ak23@gmail.com>
2024-01-27 18:30:29 +0530

    Difference between Flux and List

---
+++ Reactive/src/main/java/com/ak/reactive/utils/lectures/section02/Lec07FluxVsList.java
@@ -0,0 +1,27 @@
+package com.ak.reactive.utils.lectures.section02;
+
+import com.ak.reactive.utils.Util;
+import com.ak.reactive.utils.lectures.section02.helper.NameGenerator;
+
+import java.util.List;
+
+/**
+ * We will compare in this example how a Flux can provide a better client experience compared to the traditional way
+ * of handling the data and its processing by the client
+ */
+public class Lec07FluxVsList {
+
+    public static void main(String[] args) {
+        // This implementation will pause for 5 seconds before the list is received and processed by the client
+        System.out.println("****** Traditional List processing ******");
+        List<String> names = NameGenerator.getNamesTraditional(5);
+        System.out.println(names);
+
+        // Same processing of the data elements (in the list) but with Flux
+        System.out.println("****** Flux data processing ******");
+        NameGenerator.getNames(5).subscribe(Util.onNext());
+
+        // You can clearly see that in the second case (above invocation) the client frustration is much less than the
+        // traditional one.
+    }
+}
