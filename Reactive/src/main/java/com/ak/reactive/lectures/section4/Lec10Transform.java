096a429eed6f631796ea56998a84fe209b2b095f
Abhay Kulkarni <developer.ak23@gmail.com>
2024-02-06 23:50:13 +0530

    added an example of transform operator

--- Reactive/src/main/java/com/ak/reactive/lectures/section4/Lec10Transform.java
+++ Reactive/src/main/java/com/ak/reactive/lectures/section4/Lec10Transform.java
@@ -1,11 +1,30 @@
 package com.ak.reactive.lectures.section4;
 
+import com.ak.reactive.utils.Util;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
 public class Lec10Transform {
 
     public static void main(String[] args) {
 
+        getPerson()
+                .transform(applyFilterMap())
+                .subscribe(Util.getSubscriber());
 
     }
 
+    public static Flux<Person> getPerson() {
+        return Flux.range(1, 10)
+                .map(i -> new Person());
+    }
 
+    public static Function<Flux<Person>, Flux<Person>> applyFilterMap() {
+        return personFlux -> personFlux
+                .filter(p -> p.getAge() > 10)
+                .doOnNext(p -> p.setName(p.getName().toUpperCase()))
+                .doOnDiscard(Person.class, p -> System.out.println("Not allowing:" + p))
+                ;
+    }
 }
---
+++ Reactive/src/main/java/com/ak/reactive/lectures/section4/Person.java
@@ -0,0 +1,18 @@
+package com.ak.reactive.lectures.section4;
+
+import com.ak.reactive.utils.Util;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        this.name = Util.faker().name().firstName();
+        this.age = Util.faker().random().nextInt(1, 30);
+    }
+}
