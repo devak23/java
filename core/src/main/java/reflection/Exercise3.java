054909f2706624d57719e1f7920f7ce13e133b0c
Abhay Kulkarni <developer.ak23@gmail.com>
2024-05-26 23:21:10 +0530

    fixed exercise

--- core/src/main/java/reflection/Exercise3.java
+++ core/src/main/java/reflection/Exercise3.java
@@ -6,15 +6,24 @@ import java.util.Arrays;
 
 public class Exercise3 {
 
+    public static final byte[] BYTES = new byte[200];
+
     public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        ImageBuffer imageBuffer = createObject(ImageBuffer.class, 200);
-        System.out.println(imageBuffer);
+        ImageBuffer imageBuffer1 = createObject(ImageBuffer.class, 200);
+        System.out.println(imageBuffer1);
+
+        ImageBuffer imageBuffer2 = createObject(ImageBuffer.class, BYTES);
+        System.out.println(imageBuffer2);
+
+        ImageBuffer imageBuffer3 = createObject(ImageBuffer.class);
+        System.out.println(imageBuffer3);
+
     }
 
     public static <T> T createObject(Class<T> clazz, Object ...args) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         Class<?>[] parameterTypes = Arrays.stream(args).map(Object::getClass).toArray(Class[]::new);
-        Constructor<?> constructor = clazz.getConstructor(parameterTypes);
-
+        Constructor<?> constructor = clazz.getDeclaredConstructor(parameterTypes);
+        constructor.setAccessible(true);
         return (T) constructor.newInstance(args);
     }
 }
