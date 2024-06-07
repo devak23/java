a17cc41d4ec694d1f0befd4a4476415cc88d1ebf
Abhay Kulkarni <developer.ak23@gmail.com>
2024-06-08 01:41:44 +0530

    minor fixes

--- microservices-parent/inventory-service/src/main/java/com/rnd/app/purchase/inventoryservice/InventoryServiceApplication.java
+++ microservices-parent/inventory-service/src/main/java/com/rnd/app/purchase/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.rnd.app.purchase.inventoryservice;
 
+import com.rnd.app.purchase.inventoryservice.model.Inventory;
+import com.rnd.app.purchase.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,24 +14,24 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-//		return args -> {
-//			Inventory iPhone = new Inventory();
-//			iPhone.setSkuCode("iPhone13");
-//			iPhone.setQuantity(100);
-//
-//			Inventory samsung = new Inventory();
-//			samsung.setSkuCode("Samsung S23");
-//			samsung.setQuantity(200);
-//
-//			Inventory motorola = new Inventory();
-//			motorola.setSkuCode("Motorola Razr");
-//			motorola.setQuantity(0);
-//
-//			inventoryRepository.save(iPhone);
-//			inventoryRepository.save(samsung);
-//			inventoryRepository.save(motorola);
-//		};
-//	}
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory iPhone = new Inventory();
+			iPhone.setSkuCode("iPhone13");
+			iPhone.setQuantity(100);
+
+			Inventory samsung = new Inventory();
+			samsung.setSkuCode("Samsung S23");
+			samsung.setQuantity(200);
+
+			Inventory motorola = new Inventory();
+			motorola.setSkuCode("Motorola Razr");
+			motorola.setQuantity(0);
+
+			inventoryRepository.save(iPhone);
+			inventoryRepository.save(samsung);
+			inventoryRepository.save(motorola);
+		};
+	}
 }
