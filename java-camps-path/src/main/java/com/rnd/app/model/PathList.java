2d213805b2cb506d39c4c64d8640a1e78baa1af2
Abhay Kulkarni <developer.ak23@gmail.com>
2024-06-29 18:30:42 +0530

    minor update

--- java-camps-path/src/main/java/com/rnd/app/model/PathList.java
+++ java-camps-path/src/main/java/com/rnd/app/model/PathList.java
@@ -3,15 +3,21 @@ package com.rnd.app.model;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public record PathList(List<Path> paths) {
+
     public Optional<Path> shortestPath() {
-        return paths.parallelStream().min(Comparator.comparing(Path::distance));
+        return shortestPathWithCondition(p -> true);
     }
 
-    public Optional<Path> shortestPath(List<Point> includingPoint) {
+    public Optional<Path> shortestPath(List<Point> points) {
+        return shortestPathWithCondition(p -> p.includesPoints(points));
+    }
+
+    public Optional<Path> shortestPathWithCondition(Predicate<Path> condition) {
         return paths.parallelStream()
-                .filter(p -> p.includesPoints(includingPoint))
+                .filter(condition)
                 .min(Comparator.comparing(Path::distance));
     }
 }
