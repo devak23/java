5b752bee07e8a66d0c3efe8ee7ad470e99d35371
abhayk <developer.ak23@gmail.com>
2024-08-12 23:39:46 +0530

    a little more example of teeing and Collectors

--- core/src/main/java/streams/StreamsMain.java
+++ core/src/main/java/streams/StreamsMain.java
@@ -14,6 +14,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -84,12 +85,15 @@ public class StreamsMain {
                 ));
         System.out.println(nameMap);
 
-        // Another way and probablym more efficient way to achieve the above
+        // Another way and probably more efficient way to achieve the above
         Map<String, Long> nameCountMap = names.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         System.out.println(nameCountMap);
 
-        // Using Teeing to find the max and min marks of a Student
+        // Using Teeing to find the max and min marks of a Student. Starting with JDK 12, we can merge the results of
+        // two collectors via Collectors.teeing(). The result is a Collector that is a composite of two passed downstream
+        // collectors. Every element that's passed to the resulting collector is processed by both downstream collectors,
+        // and then their results are merged into the final result using the specified BiFunction.
         List<Student> students = List.of(
                 Student.builder().name("abhay").id(1).marks(91).build(),
                 Student.builder().name("manik").id(2).marks(96).build(),
@@ -123,6 +127,52 @@ public class StreamsMain {
 
         System.out.println("Total number of distinct characters in the name list: " + names.stream().distinct().mapToInt(String::length).sum());
 
+        // Yet another teeing example:
+        CountSum countSum = Stream.of(2, 11, 1, 5, 7, 8, 12)
+                .collect(Collectors.teeing(
+                        Collectors.counting(),
+                        Collectors.summingInt(num -> num),
+                        CountSum::new
+                ));
+        System.out.println("Count = " + countSum.count + ", Sum = " + countSum.sum);
+        // Here, we have applied two collectors to each element from the stream (counting() and summingInt()) and the
+        // results have been merged in an instance of CountSum
+
+        // Another problem: Find the min and max of a given stream of numbers
+        MinMax minMax = Stream.of(1, 3, -2, 34, 21, 21, -22, 21, 56, 33, -45).collect(Collectors.teeing(
+                Collectors.minBy(Comparator.naturalOrder()),
+                Collectors.maxBy(Comparator.naturalOrder()),
+                (Optional<Integer> num1, Optional<Integer> num2) -> new MinMax(num1.orElse(Integer.MIN_VALUE), num2.orElse(Integer.MAX_VALUE))
+        ));
+        System.out.println("Max: " + minMax.max + ", Min: " + minMax.min);
+
+        // Let's have another one:
+        Map<Double, Long> elevations = DoubleStream.of(
+                22, -10, 100, -5, 100, 123, 123, 22, 230, -1, -21, 250, 22, 21, -34, -12)
+                .filter(e -> e > 0)
+                .map(e -> e * 0.393701)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("elevations = " + elevations);
+
+        // We start with a list of elevations given in centimeters (based on sea level being 0). We first filter the
+        // negative elevations via filter, then convert them into inches via map, and then we count them via groupingBy
+        // and counting.
+
+        // The resulting data is carried by Map<Double, Long>, which is not very expressive. If we pull this map out of
+        // the context (for instance, we pass it as an argument into a method), it is hard to say what Double and Long
+        // represent. It would be more expressive to have something such as Map<Elevation, ElevationCount>, which
+        // clearly describes its content. Using the record classes Elevation and Elevation count, the same code now looks
+        // much better!
+        Map<Elevation, ElevationCount> elevationCountMap = DoubleStream.of(
+                22, -10, 100, -5, 100, 123, 22, 230, -1, 250, 22)
+                .filter(e -> e > 0)
+                .mapToObj(Elevation::new)
+                .collect(Collectors.groupingBy(Function.identity()
+                        , Collectors.collectingAndThen(Collectors.counting(), ElevationCount::new))
+                );
+
+
         // find the longest name in the list
         String longestName = names.stream().reduce((n1, n2) -> n1.length() > n2.length() ? n1 : n2).orElseThrow();
         String shortestName = names.stream().reduce((n1, n2) -> n1.length() < n2.length() ? n1 : n2).orElseThrow();
@@ -153,4 +203,24 @@ public class StreamsMain {
         );
         System.out.println(languageNames);
     }
+
+    // The following class simply stores the number of elements in a stream of integers and their sum
+    static class CountSum {
+        private final Long count;
+        private final Integer sum;
+        public CountSum(Long count, Integer sum) {
+            this.count = count;
+            this.sum = sum;
+        }
+    }
+
+    record MinMax(Integer min, Integer max) {}
+
+    record Elevation(double value) {
+        Elevation(double value) {
+            this.value = value * 0.393701;
+        }
+    }
+
+    record ElevationCount(long count) {}
 }
