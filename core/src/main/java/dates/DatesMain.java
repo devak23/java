820bd52342562bfec1aa76e40e4b7e2d95ffe363
abhayk <developer.ak23@gmail.com>
2024-08-21 22:59:40 +0530

    minor refactoring

--- core/src/main/java/dates/DatesMain.java
+++ core/src/main/java/dates/DatesMain.java
@@ -10,17 +10,17 @@ import java.util.function.BiFunction;
 @Slf4j
 public class DatesMain {
     public static BiFunction<Date, ZoneId, String> toDaysPeriod = Dates::toDayPeriod;
-    public static BiFunction<Date, ZoneId, String> getToDaysPeriodNewPattern = Dates::toDayPeriodWithNewPattern;
+    public static BiFunction<Date, ZoneId, String> toDayPeriodWithNewPattern = Dates::toDayPeriodWithNewPattern;
 
     public static void main(String[] args) {
         printDates(toDaysPeriod);
-        printDates(getToDaysPeriodNewPattern);
+        printDates(toDayPeriodWithNewPattern);
     }
 
     public static void printDates(BiFunction<Date, ZoneId, String> func) {
         log.info("====================================================");
-        List<String> zoneIds = List.of("America/Los_Angeles", "America/Montreal", "Africa/Casablanca", "Europe/Berlin","Asia/Kolkata", "Japan", "Australia/Sydney");
-        Date now = new Date();
-        zoneIds.forEach(z -> log.info("{}:  {}",z, func.apply(now ,ZoneId.of(z))));
+        List<String> zoneIds = List.of("America/Los_Angeles", "America/Montreal", "Africa/Casablanca"
+                , "Europe/Berlin","Asia/Kolkata", "Japan", "Australia/Sydney");
+        zoneIds.forEach(z -> log.info("{}:  {}",z, func.apply(new Date(), ZoneId.of(z))));
     }
 }
