2903c9432e6353eded4e7b56abdb669895cacceb
abhayk <developer.ak23@gmail.com>
2024-09-21 21:55:17 +0530

    structured scope task with shutdown on failure

--- concurrency/src/main/java/com/ak/learning/concurrency/virtualthreads/StructuredTaskScopeWithShutdownMain.java
+++ concurrency/src/main/java/com/ak/learning/concurrency/virtualthreads/StructuredTaskScopeWithShutdownMain.java
@@ -1,5 +1,6 @@
 package com.ak.learning.concurrency.virtualthreads;
 
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.logging.Logger;
@@ -17,11 +18,49 @@ public class StructuredTaskScopeWithShutdownMain {
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tT] [%4$-7s] %5$s %n");
 
-        TestingTeam team = buildTestingTeam();
-        LOGGER.info(STR."Team: \{team}");
+        TestingTeam team = buildTestingTeamWithAtLeastOneSuccess();
+        LOGGER.info(STR."At least one success: \{team}");
+
+        TestingTeam team2 = buildTestingTeamWithAllSuccess();
+        LOGGER.info(STR."Result: \{team2}");
     }
 
-    private static TestingTeam buildTestingTeam() {
+    // Here we intentionally replaced ID 3 with Integer.MAX_VALUE. Since there is no tester with this ID, the server
+    // will throw UserNotFoundException. This means that the states of the subtasks will reveal that the third subtask
+    // has failed
+    private static TestingTeam buildTestingTeamWithAllSuccess() {
+        try (StructuredTaskScope.ShutdownOnFailure scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            StructuredTaskScope.Subtask subtask1 = scope.fork(() -> fetchTester(1));
+            StructuredTaskScope.Subtask subtask2 = scope.fork(() -> fetchTester(2));
+            StructuredTaskScope.Subtask subtask3 = scope.fork(() -> fetchTester(Integer.MAX_VALUE));
+
+            scope.join();
+            LOGGER.info("subtask1-state: " + subtask1.state());
+            LOGGER.info("subtask2-state: " + subtask2.state());
+            LOGGER.info("subtask3-state: " + subtask3.state());
+
+            Optional<Throwable> exception = scope.exception();
+            if (exception.isEmpty()) {
+                return new TestingTeam((String) subtask1.get(), (String) subtask2.get(), (String) subtask3.get());
+            } else {
+                return new TestingTeam(exception.get().getMessage());
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // OUTPUT:
+    // [21:53:16] [INFO   ] sleeping arbitrarily: VirtualThread[#42]/runnable@ForkJoinPool-1-worker-1
+    // [21:53:16] [INFO   ] sleeping arbitrarily: VirtualThread[#44]/runnable@ForkJoinPool-1-worker-2
+    // [21:53:16] [INFO   ] sleeping arbitrarily: VirtualThread[#43]/runnable@ForkJoinPool-1-worker-4
+    // [21:53:19] [INFO   ] subtask1-state: SUCCESS
+    // [21:53:19] [INFO   ] subtask2-state: SUCCESS
+    // [21:53:19] [INFO   ] subtask3-state: FAILED
+    // [21:53:19] [INFO   ] Result: TestingTeam(testers=[Code: 404])
+
+
+    private static TestingTeam buildTestingTeamWithAtLeastOneSuccess() {
         try (StructuredTaskScope.ShutdownOnSuccess scope = new StructuredTaskScope.ShutdownOnSuccess()) {
             StructuredTaskScope.Subtask<String> subtask1 = scope.fork(() -> fetchTester(1));
             StructuredTaskScope.Subtask<String> subtask2 = scope.fork(() -> fetchTester(2));
