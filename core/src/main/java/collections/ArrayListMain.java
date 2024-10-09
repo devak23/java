1bfc633a3c3910980c56cde54041b2d3e0b12b2c
abhayk <developer.ak23@gmail.com>
2024-10-09 09:28:28 +0530

    some more modification

--- core/src/main/java/collections/ArrayListMain.java
+++ core/src/main/java/collections/ArrayListMain.java
@@ -11,38 +11,39 @@ public class ArrayListMain {
 
     public static void main(String[] args) {
         List<String> friends = getFriends();
-        String expectedOutput = "Dhiren,Meenakshi,Nimisha,Saif";
+        String expectedOutput1 = "Dhiren,Meenakshi,Nimisha,Saif";
 
 
         IntStream.range(0, 1000).forEach(i -> {
-            String returnedString = iterateAndBuildString(friends);
-            if (!returnedString.equals(expectedOutput)) {
-                throw new RuntimeException("ArrayList DOES NOT maintain order while retrieving");
-            }
+            performTest(expectedOutput1, friends);
         });
         log.info("ArrayList MAINTAINS the order of elements while retrieval");
-
-
 
         log.info("=========================dynamic loading===========================================");
 
         String expectedOutput2 = "Suhas,Kavita,Abrar,Varkha";
         IntStream.range(0, 1000).forEach(i -> {
-            List<String> friends2 = new ArrayList<>(10);
-            friends2.add("Suhas");
-            friends2.add("Kavita");
-            friends2.add("Abrar");
-            friends2.add("Varkha");
-
-
-            String returnedString = iterateAndBuildString(friends2);
-            if (!returnedString.equals(expectedOutput2)) {
-                throw new RuntimeException("ArrayList DOES NOT maintain order during retrieval when dynamically added the elements");
-            }
+            performTest(expectedOutput2, null);
         });
 
         log.info("ArrayList MAINTAINS the order of elements while retrieval even if the elements are added dynamically");
 
+    }
+
+    private static void performTest(String expectedOutput, List<String> friends) {
+        if (friends == null) {
+            friends = new ArrayList<>(10);
+            friends.add("Suhas");
+            friends.add("Kavita");
+            friends.add("Abrar");
+            friends.add("Varkha");
+        }
+
+
+        String returnedString = iterateAndBuildString(friends);
+        if (!returnedString.equals(expectedOutput)) {
+            throw new RuntimeException("ArrayList DOES NOT maintain order during retrieval when dynamically added the elements");
+        }
     }
 
     private static String iterateAndBuildString(List<String> friends) {
