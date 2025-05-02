66a539e7dc9cd8b739fd59e69feb796cca6e66df
abhayk <developer.ak23@gmail.com>
2025-05-02 08:18:37 +0530

    functional way of filtering

---
+++ core/src/main/java/functional/FilterBooksByFilterOptions.java
@@ -0,0 +1,54 @@
+package functional;
+
+import functional.model.Book;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+
+public class FilterBooksByFilterOptions {
+
+    static class FilterOptions {
+        String genre;
+        Float rating;
+        String letter;
+
+        public FilterOptions withGenre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public FilterOptions withRating(float rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public FilterOptions withLetter(String letter) {
+            this.letter = letter;
+            return this;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FilterBooksByFilterOptions demo = new FilterBooksByFilterOptions();
+
+        List<Book> books = FilterBooksByPredicate.getBooks();
+        List<Book> filteredBooks = demo.curriedFilter()
+                .apply(books)
+                .apply(new FilterOptions()
+                        .withLetter("m")
+                        .withRating(4.3f)
+                );
+
+        filteredBooks.forEach(System.out::println);
+    }
+
+    private Function<List<Book>, Function<FilterOptions, List<Book>>> curriedFilter() {
+        return books -> options ->
+                books.stream()
+                        .filter(book -> options.genre == null || book.getGenre().strip().toLowerCase().contains(options.genre.strip().toLowerCase()))
+                        .filter(book -> options.rating == null || book.getRating() >= options.rating)
+                        .filter(book -> options.letter == null || book.getTitle().strip().toLowerCase().charAt(0) < options.letter.strip().toLowerCase().charAt(0))
+                        .toList();
+    }
+}
